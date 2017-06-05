2fdfc537583ed1265af93e18fb5722c6e2486bbc
Martyna Stepien <martyna.stepien@ldntranml010706.local>
2017-06-05 15:31:43 +0100

    main.css file cleaned from unnecessary classes main.js file updated, variables for roman, arabic and button created, onclick function added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+var userInput = document.getElementById('userInput').value;
+var calculate = document.getElementById('calculate');
+var roman = document.getElementById('roman');
+
+calculate.onclick = function(){
+  roman.innerHTML = convertToRoman();
+};
+
+function convertToRoman() {
+  console.log("Clicked");
+};
