495e237de2827e65db8938fbd834e66316a924f6
Martyna Stepien <martyna.stepien@ldntranml010706.local>
2017-06-06 12:01:08 +0100

    function converts numbers up to 20

--- test/spec/test.js
+++ test/spec/test.js
@@ -18,9 +18,24 @@
       it('converts "4" to "IV"', function () {
         expect(convertToRoman(4)).to.equal('IV');
       });
-      // it('converts "5" to "V"', function () {
-      //   expect(convertToRoman(5)).to.equal('V');
-      // });
+      it('converts "5" to "V"', function () {
+        expect(convertToRoman(5)).to.equal('V');
+      });
+      it('converts "6" to "VI"', function () {
+        expect(convertToRoman(6)).to.equal('VI');
+      });
+      it('converts "7" to "VII"', function () {
+        expect(convertToRoman(7)).to.equal('VII');
+      });
+      it('converts "9" to "IX"', function () {
+        expect(convertToRoman(9)).to.equal('IX');
+      });
+      it('converts "10" to "X"', function () {
+        expect(convertToRoman(10)).to.equal('X');
+      });
+      it('converts "11" to "XI"', function () {
+        expect(convertToRoman(11)).to.equal('XI');
+      });
     });
   });
 })();
