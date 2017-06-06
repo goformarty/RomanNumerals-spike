bdae89f0526ccfec612120e5d4a3763a5d0bb3e7
Martyna Stepien <martyna.stepien@ldntranml010706.local>
2017-06-06 10:06:12 +0100

    tests for 1,2 and 3 added

--- test/spec/test.js
+++ test/spec/test.js
@@ -3,8 +3,14 @@
 
   describe('Roman numerals', function () {
     describe('convert arabic numbers to roman numerals', function () {
-      it('converts arabic to roman', function () {
-        expect(convertToRoman(1).toequal('I'));
+      it('converts "1" to "I"', function () {
+        expect(convertToRoman(1)).to.equal('I');
+      });
+      it('converts "2" to "II"', function () {
+        expect(convertToRoman(2)).to.equal('II');
+      });
+      it('converts "3" to "III"', function () {
+        expect(convertToRoman(3)).to.equal('III');
       });
     });
   });
